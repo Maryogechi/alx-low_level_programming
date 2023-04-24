6bd4af3029299b25b198b2ac40ea9778027888e4
Maryogechi <iheanaetumaryogechi@gmail.com>
2023-04-24 06:08:34 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int di;
+for (di = '0'; di <= '9'; di++)
+putchar(di);
+putchar('\n');
+return (0);
+}
