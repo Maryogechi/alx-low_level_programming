fb258b4e9f6e96970c6ee93c4f08c6175f3395dd
Maryogechi <iheanaetumaryogechi@gmail.com>
2023-04-26 14:52:02 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ * must be follow a new line
+ * use _putchar function twice only
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+        char low;
+        int i = 0;
+        while (i <= 9)
+        {
+        for (low = 'a'; low <= 'z'; low++)
+                _putchar(low);
+        _putchar('\n');
+        i++;
+        }
+}
