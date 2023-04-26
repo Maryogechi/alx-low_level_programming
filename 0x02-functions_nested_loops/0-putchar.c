7491acdc0596000e09bacafd3a94ba8c4a1e283c
Maryogechi <iheanaetumaryogechi@gmail.com>
2023-04-26 14:41:48 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * main - prints _putchar
+ * must be on a new line
+ * use _putchar function only
+ *
+ * Return: 0 Always (yes)
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
